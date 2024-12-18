b67b78f0e5b7c98eee57cbfdc741f2f2e39d78c8
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-19 00:03:14 +0300

    Add files via upload

---
+++ dkr2n1.pas
@@ -0,0 +1,31 @@
+﻿program dkr2n1;
+const  MAX = 10;
+type TArray = array[1..MAX] of Integer;
+var arr: TArray; n, i, j: Integer; count: array[1..MAX] of Integer; visited: array[1..MAX] of Boolean;
+begin
+  n:=10;
+  WriteLn('Введите элементы массива:');
+  for i := 1 to n do
+    Readln(arr[i]);
+  for i := 1 to n do
+  begin
+    visited[i] := False;
+    count[i] := 0;
+  end;
+  for i := 1 to n do
+  begin
+    if not visited[i] then
+    begin
+      count[i] := 1;
+      for j := i + 1 to n do
+      begin
+        if arr[i] = arr[j] then
+        begin
+          count[i] := count[i] + 1;
+          visited[j] := True;
+        end;
+      end;
+      WriteLn('Элемент ', arr[i], ' встречается ', count[i], ' раз');
+    end;
+  end;
+end.
---
+++ dkr2n2.pas
@@ -0,0 +1,46 @@
+﻿program dkr2n2;
+const MAX_WORDS = 100;
+var inputStr: string; words: array[1..MAX_WORDS] of string; count: array[1..MAX_WORDS] of Integer; n, i, j, maxCount, maxIndex: Integer; currentWord: string;
+begin
+  n := 0;
+  maxCount := 0;
+  maxIndex := -1;
+  Write('Введите строку: ');
+  ReadLn(inputStr);
+  inputStr := inputStr + ' ';
+  currentWord := '';
+  for i := 1 to Length(inputStr) do
+  begin
+    if not (inputStr[i] in [' ', ',', '.']) then
+      currentWord := currentWord + inputStr[i]
+    else if currentWord <> '' then
+    begin
+      for j := 1 to n do
+      begin
+        if words[j] = currentWord then
+        begin
+          Inc(count[j]);
+          currentWord := '';
+          Break;
+        end;
+      end;
+      if currentWord <> '' then
+      begin
+        Inc(n);
+        words[n] := currentWord;
+        count[n] := 1;
+        currentWord := '';
+      end;
+    end;
+  end;
+  for i := 1 to n do
+    if count[i] > maxCount then
+    begin
+      maxCount := count[i];
+      maxIndex := i;
+    end;
+  if maxIndex <> -1 then
+    WriteLn('Слово "', words[maxIndex], '" встречается ', maxCount, ' раз.')
+  else
+    WriteLn('В строке нет слов.');
+end.
